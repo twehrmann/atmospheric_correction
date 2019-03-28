1be94442bf888ef4facf726e9ff0b43962962c00
twehrmann <thilo.wehrmann@eoss.cloud>
2019-03-28 20:25:21 +0100

    set to https://public.eoss...

--- SIAC/globals.py
+++ SIAC/globals.py
@@ -2,11 +2,11 @@
 
 emus_dir = 'SIAC/emus/',
 EMU_URL = 'http://www2.geog.ucl.ac.uk/~ucfafyi/emus/'
-EMU_URL = 'http://public.eoss.cloud.s3.amazonaws.com/data/emus/'
+EMU_URL = 'https://s3-eu-west-1.amazonaws.com/public.eoss.cloud/data/emus/'
 
 mcd43_dir = '~/DATA/Multiply/MCD43/'
 global_dem = '/vsicurl/http://www2.geog.ucl.ac.uk/~ucfafyi/eles/global_dem.vrt'
-global_dem = '/vsicurl/https://public.eoss.cloud.s3.amazonaws.com/data/gdem/global_dem.vrt'
+global_dem = '/vsicurl/https://s3-eu-west-1.amazonaws.com/public.eoss.cloud/data/gdem/global_dem.vrt'
 
 cams_dir = '/vsicurl/http://www2.geog.ucl.ac.uk/~ucfafyi/cams/'
 spec_m_dir = 'SIAC/spectral_mapping'
