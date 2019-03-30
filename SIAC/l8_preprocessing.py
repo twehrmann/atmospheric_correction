83a7a02b0accb5a3d286311e4436fdd3f6257e91
thilowehrmann <twehrmann@conabio.gob.mx>
2019-03-30 18:46:07 +0100

    scan for mtl text file

--- SIAC/l8_preprocessing.py
+++ SIAC/l8_preprocessing.py
@@ -10,7 +10,7 @@ def l8_pre_processing(l8_dir):
         if len(filenames) > 0:
             temp = [dirpath + '/' + i for i in filenames]
             for j in temp:
-                if 'mtl.' in j.lower():
+                if 'mtl.txt' in j.lower():
                     metafiles.append(j)
     l8_tiles = []
     for metafile in metafiles:
